0e500f2c036cf3f97555d6d9ae803a8d923b9904
Tomislav Bisko <tomislav.bisko@gmail.com>
2015-01-11 17:56:39 +0100

    Initial commit.

---
+++ RosesGUI/Analizer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Imaging.Filters;
+using AForge;
+using AForge.Imaging;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Interop;
+using System.Windows;
+using System.Drawing.Imaging;
+using System.Windows.Threading;
+
+namespace WpfApplication1
+{
+    class Analizer
+    {
+        // U ovom zadatku koristi se aforge biblioteka za obradu slika u C# jeziku
+        // Za zadanu sliku rjesenje je 5.
+        // Rezultatna slika nalazi se na web adresi:
+        // http://imageshack.us/scaled/landing/221/merged.png
+        public static Bitmap FindObjects(System.Drawing.Bitmap image, int hueMin, int hueMax, float saturationMin, float saturationMax, float luminanceMin, float luminanceMax, int blobWidth, int blobHeight)
+        {
+            // HSL filter
+            HSLFiltering filter = new HSLFiltering();
+
+            // Zelimo zadrzati samo crvenu
+            filter.Hue = new IntRange(hueMin, hueMax);
+            filter.Saturation = new Range(saturationMin, saturationMax);
+            filter.Luminance = new Range(luminanceMin, luminanceMax);
+
+            // primjenimo filter
+            filter.ApplyInPlace(image);
+
+            // algoritam za trazenje nakupina (eng. blob)
+            BlobCounterBase bc = new BlobCounter();
+            // postavljamo filter za karakteristike nakupina koje trazimo
+            bc.FilterBlobs = true;
+            // Trazimo da budu odredene velicine kako bi uklonili sum na slici
+            bc.MinWidth = blobWidth;
+            bc.MinHeight = blobHeight;
+            bc.ObjectsOrder = ObjectsOrder.Size;
+
+
+            Bitmap redBlobs = image;
+            bc.ProcessImage(redBlobs);
+
+            //Trazimo crvene nakupine, tj crvene objekte na slici
+            Blob[] blobs = bc.GetObjectsInformation();
+            //Za zadanu sliku nasli smo ih pet, i spremljeni su u polje blobs, duzina polja je broj crvenih objekata na slici
+            //Console.WriteLine(blobs.Length);
+            bc.ExtractBlobsImage(redBlobs, blobs[0], true);
+
+            //DODATNO: Radimo sliku samo sa pronadenim objektima, ovo nam sluzi samo kao provjera
+            Merge roses = new Merge();
+            Bitmap resultImage = blobs[0].Image.ToManagedImage();
+
+            for (int j = 1; j < blobs.Length; j++)
+            {
+                bc.ExtractBlobsImage(redBlobs, blobs[j], true);
+                roses.OverlayImage = blobs[j].Image.ToManagedImage();
+                resultImage = roses.Apply(resultImage);
+            }
+            //Odkomentirati sljedecu liniju, kako bi slika sa pronadenim objektima bila spremljena na disk, te se onda moze pregledati
+            //resultImage.Save(@"C:\merged.jpg");
+            return resultImage;
+
+        }
+
+        public static Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            // BitmapImage bitmapImage = new BitmapImage(new Uri("../Images/test.png", UriKind.Relative));
+
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public static BitmapSource CreateBitmapSourceFromBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            if (Application.Current.Dispatcher == null)
+                return null; // Is it possible?
+
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    // You need to specify the image format to fill the stream. 
+                    // I'm assuming it is PNG
+                    bitmap.Save(memoryStream, ImageFormat.Png);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    // Make sure to create the bitmap in the UI thread
+                    if (InvokeRequired)
+                        return (BitmapSource)Application.Current.Dispatcher.Invoke(
+                            new Func<Stream, BitmapSource>(CreateBitmapSourceFromBitmap),
+                            DispatcherPriority.Normal,
+                            memoryStream);
+
+                    return CreateBitmapSourceFromBitmap(memoryStream);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static bool InvokeRequired
+        {
+            get { return Dispatcher.CurrentDispatcher != Application.Current.Dispatcher; }
+        }
+
+        private static BitmapSource CreateBitmapSourceFromBitmap(Stream stream)
+        {
+            BitmapDecoder bitmapDecoder = BitmapDecoder.Create(
+                stream,
+                BitmapCreateOptions.PreservePixelFormat,
+                BitmapCacheOption.OnLoad);
+
+            // This will disconnect the stream from the image completely...
+            WriteableBitmap writable = new WriteableBitmap(bitmapDecoder.Frames.Single());
+            writable.Freeze();
+
+            return writable;
+        }
+    }
+}
---
+++ RosesGUI/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static String imageURI = @"pack://application:,,,/Images/roses.jpg";
+        private static Bitmap resultImage = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            imageEnd.Source = Analize(imageURI);
+            tabItem3.IsSelected = true;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                imageURI = dlg.FileName;
+                label1.Content = imageURI;
+                imageEnd.Source = Analize(imageURI);
+            }
+        }
+
+        private BitmapSource Analize(String filename)
+        {
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = new Uri(filename, UriKind.RelativeOrAbsolute);
+            src.CacheOption = BitmapCacheOption.OnLoad;
+            src.EndInit();
+            imageStart.Source = src;
+            imageStart.Stretch = Stretch.Fill;
+
+            int hueMin = Int16.Parse(textBox1.Text);
+            int hueMax = Int16.Parse(textBox2.Text);
+            float saturationMin = Single.Parse(textBox3.Text, CultureInfo.InvariantCulture);
+            float saturationMax = Single.Parse(textBox4.Text, CultureInfo.InvariantCulture);
+            float luminanceMin = Single.Parse(textBox5.Text, CultureInfo.InvariantCulture);
+            float luminanceMax = Single.Parse(textBox6.Text, CultureInfo.InvariantCulture);
+
+            int blobWidth = Int16.Parse(textBox7.Text);
+            int blobHeight = Int16.Parse(textBox8.Text);
+
+            resultImage = Analizer.FindObjects(Analizer.BitmapImage2Bitmap(src), hueMin, hueMax, saturationMin, saturationMax, luminanceMin, luminanceMax, blobWidth, blobHeight);
+            //Bitmap resultImage = Analizer.FindObjects(image);
+            return Analizer.CreateBitmapSourceFromBitmap(resultImage);
+        }
+
+        private void textBox7_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+                e.Handled = isNumber(e.Text);
+        }
+
+        private bool isNumber(String input) {
+            Regex regex = new Regex("[^0-9]+");
+            return regex.IsMatch(input);
+        }
+
+        private bool hasDotInString(String text) {
+                return text.Contains(".");
+        }
+
+        private void textBox8_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = isNumber(e.Text);
+        }
+
+        private void textBox1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = isNumber(e.Text);
+        }
+
+        private void textBox2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = isNumber(e.Text);
+        }
+
+        private void textBox3_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Equals(".") && textBox3.Text.Length > 0 && !hasDotInString(textBox3.Text))
+            {
+                e.Handled = false;
+            }
+            else {
+                e.Handled = isNumber(e.Text);
+            }
+    
+        }
+
+        private void textBox4_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Equals(".") && textBox3.Text.Length > 0 && !hasDotInString(textBox3.Text))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = isNumber(e.Text);
+            }
+        }
+
+        private void textBox5_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Equals(".") && textBox3.Text.Length > 0 && !hasDotInString(textBox3.Text))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = isNumber(e.Text);
+            }
+        }
+
+        private void textBox6_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Equals(".") && textBox3.Text.Length > 0 && !hasDotInString(textBox3.Text))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = isNumber(e.Text);
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultImage != null) 
+            {
+                resultImage.Save(@"C:\merged.jpg");
+            }
+        }
+    }
+}
